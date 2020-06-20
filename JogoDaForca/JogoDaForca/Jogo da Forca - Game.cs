6dbbd434062579df7097e100ff47c73c2cd29b19
Matheus de Amorim Pacheco <matheus-amorim14@hotmail.com>
2020-06-20 18:21:30 -0300

    creadaJaneladeJogo

---
+++ JogoDaForca/JogoDaForca/Jogo da Forca - Game.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaForca
+{
+    public partial class Jogo_da_Forca___Game : Form
+    {
+        public Jogo_da_Forca___Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Jogo_da_Forca___Game_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- JogoDaForca/JogoDaForca/Menu.Designer.cs
+++ JogoDaForca/JogoDaForca/Menu.Designer.cs
@@ -42,53 +42,58 @@
             this.botaoJogar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.botaoJogar.FlatAppearance.BorderSize = 0;
             this.botaoJogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.botaoJogar.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botaoJogar.Location = new System.Drawing.Point(12, 109);
+            this.botaoJogar.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoJogar.Location = new System.Drawing.Point(18, 168);
+            this.botaoJogar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.botaoJogar.Name = "botaoJogar";
-            this.botaoJogar.Size = new System.Drawing.Size(170, 66);
+            this.botaoJogar.Size = new System.Drawing.Size(255, 102);
             this.botaoJogar.TabIndex = 0;
             this.botaoJogar.Text = "Jogar";
             this.botaoJogar.UseVisualStyleBackColor = false;
             // 
             // escolhaBancoPalavras
             // 
-            this.escolhaBancoPalavras.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.escolhaBancoPalavras.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.escolhaBancoPalavras.FormattingEnabled = true;
-            this.escolhaBancoPalavras.ItemHeight = 30;
-            this.escolhaBancoPalavras.Location = new System.Drawing.Point(460, 141);
+            this.escolhaBancoPalavras.ItemHeight = 44;
+            this.escolhaBancoPalavras.Location = new System.Drawing.Point(690, 217);
+            this.escolhaBancoPalavras.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.escolhaBancoPalavras.Name = "escolhaBancoPalavras";
-            this.escolhaBancoPalavras.Size = new System.Drawing.Size(328, 34);
+            this.escolhaBancoPalavras.Size = new System.Drawing.Size(490, 48);
             this.escolhaBancoPalavras.TabIndex = 1;
             // 
             // textoLBEscolhaBancoPalavra
             // 
             this.textoLBEscolhaBancoPalavra.AutoSize = true;
-            this.textoLBEscolhaBancoPalavra.Font = new System.Drawing.Font("Showcard Gothic", 14.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textoLBEscolhaBancoPalavra.Location = new System.Drawing.Point(278, 109);
+            this.textoLBEscolhaBancoPalavra.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textoLBEscolhaBancoPalavra.Location = new System.Drawing.Point(417, 168);
+            this.textoLBEscolhaBancoPalavra.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.textoLBEscolhaBancoPalavra.Name = "textoLBEscolhaBancoPalavra";
-            this.textoLBEscolhaBancoPalavra.Size = new System.Drawing.Size(510, 26);
+            this.textoLBEscolhaBancoPalavra.Size = new System.Drawing.Size(575, 37);
             this.textoLBEscolhaBancoPalavra.TabIndex = 2;
             this.textoLBEscolhaBancoPalavra.Text = "Escolha um metodo para escolher as palavras";
             // 
             // Titulo
             // 
             this.Titulo.AutoSize = true;
-            this.Titulo.Font = new System.Drawing.Font("Showcard Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Titulo.Location = new System.Drawing.Point(12, 9);
+            this.Titulo.Font = new System.Drawing.Font("Impact", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(18, 14);
+            this.Titulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Titulo.Name = "Titulo";
-            this.Titulo.Size = new System.Drawing.Size(318, 50);
+            this.Titulo.Size = new System.Drawing.Size(364, 75);
             this.Titulo.TabIndex = 3;
             this.Titulo.Text = "Jogo da Forca";
             // 
             // Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.Titulo);
             this.Controls.Add(this.textoLBEscolhaBancoPalavra);
             this.Controls.Add(this.escolhaBancoPalavras);
             this.Controls.Add(this.botaoJogar);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Menu";
             this.Text = "Jogo da Forca - Menu";
             this.Load += new System.EventHandler(this.Menu_Load);
